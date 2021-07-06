5edceb9c7cbd336750999de029e2bc45f4f9e895
Tano1990 <maurobonavota@gmail.com>
2021-07-06 20:01:53 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar.jsx';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            ENTREGABLE N° 1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GARBO
-        </a>
-      </header>
+    <div>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+//import react from 'react'
+import * as ReactBootStrap from "react-bootstrap";
+export default function Navbar(){
+return(
+<>
+ <ReactBootStrap.Navbar collapseOnSelect expand="xl" bg="info">
+  <ReactBootStrap.Navbar.Brand className="text-warning font-weight-bold" href="#home">GARBO</ReactBootStrap.Navbar.Brand>
+  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+    <ReactBootStrap.Nav className="mr-auto">
+        <ReactBootStrap.NavDropdown title="PRODUCTOS" id="collasible-nav-dropdown">
+        <ReactBootStrap.NavDropdown.Item href="#action/3.1">Difusores</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.2">Velas Aromáticas</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.3">Shampoos y Acondicionadores Sólidos</ReactBootStrap.NavDropdown.Item>
+        <ReactBootStrap.NavDropdown.Divider/>
+        <ReactBootStrap.NavDropdown.Item href="#action/3.4">Kits SPA</ReactBootStrap.NavDropdown.Item>
+      </ReactBootStrap.NavDropdown>
+    </ReactBootStrap.Nav>
+    <ReactBootStrap.Nav>
+      <ReactBootStrap.Nav.Item className="text-warning mr-4" href="#deets">ENVIOS</ReactBootStrap.Nav.Item>
+      <ReactBootStrap.Nav.Item className="text-warning mr-4" eventKey={2} href="#memes">CONTACTO</ReactBootStrap.Nav.Item>
+    </ReactBootStrap.Nav>
+    <ReactBootStrap.Nav.Item className="text-warning" bg="dark" href="#pricing">CARRITO</ReactBootStrap.Nav.Item>
+  </ReactBootStrap.Navbar.Collapse>
+ </ReactBootStrap.Navbar>
+</>
+)
+}
